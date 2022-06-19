ccf669281bf2feb0f2ab52609cc866ded62053e3
mazor <mazor@student.21-school.ru>
2022-06-20 03:48:40 +0400

    done rendering template and saving to pdf

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 URL_TEMPLATE = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/{date}/currencies/{from_currency}/{' \
-                   'to_currency}.min.json'
+               'to_currency}.min.json'
 CURRENCY_PAIRS = ('usd/rub', 'gel/rub')
 PERIOD = 30
-HTML_TEMPLATE = 'template.html'
+TEMPLATE_DIR = 'templates'
+HTML_TEMPLATE = 'currencies.html'
--- save_currencies_to_pdf.py
+++ save_currencies_to_pdf.py
@@ -2,6 +2,11 @@ import asyncio
 
 import config
 from request_currency import request_currency
+from data_processing import create_plot, save_pdf
 
 if __name__ == '__main__':
     currencies = asyncio.run(request_currency(config.CURRENCY_PAIRS, config.PERIOD))
+    data = []
+    for key, value in currencies.items():
+        data.append(create_plot(key, value))
+    save_pdf(data)
